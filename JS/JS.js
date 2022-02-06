357b9145781e349fe881dfd30f0cc9aae226b6d7
Arthur das Chagas <98226714+Etheriunm@users.noreply.github.com>
2022-02-05 22:23:47 -0300

    Repost 4

--- JS/JS.js
+++ JS/JS.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const item_menu = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+const barra = document.querySelector('.barra');
+const tex = document.querySelector('.tex');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
@@ -24,4 +26,28 @@ item_menu.forEach(item=>{
         hamburger.classList.toggle('active');
         mobile_menu.classList.toggle('active');
     })
-})
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position >= 600) {
+        barra.style.left = '0px';
+        barra.style.backgroundColor = 'black';
+    }
+    else {
+            barra.style.left = '-30vh';
+            barra.style.backgroundColor = 'transparent';
+        }
+} );
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position >= 600) {
+        tex.style.color = 'black';
+        tex.style.margin = '0 auto 0 30vh';
+    }
+    else {
+            tex.style.color = 'transparent';
+            tex.style.margin = '0 auto 0 20vh';
+        }
+} );
